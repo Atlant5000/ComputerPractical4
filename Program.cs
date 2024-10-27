21259370acfaccf7b97a626a56c8b30e89ce3d5b
Atlant5000 <maximmmd13@gmail.com>
2024-10-27 12:16:16 +0200

    ComputerPractical4

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine($"a+b+c={a+b+c}");
+            Console.ReadLine();*/
+
+            //Task 2
+            /*Console.Write("side1=");
+            double sidel = double.Parse(Console.ReadLine());
+            Console.Write("side2=");
+            double side2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"P={(side1 + side2) * 2}");
+            Console.WriteLine($"S={side1 * side2}"); Console.ReadLine();*/
+
+            //Task 3
+            /*Console.Write("leg1=");
+            int legl = int.Parse(Console.ReadLine());
+            Console.Write("leg2=");
+            int leg2= int.Parse(Console.ReadLine());
+            Console.WriteLine($"hypotenuse={Math.sqrt(leg1*leg1+leg2*leg2)}");
+            Console.ReadLine();*/
+
+            //task 4
+            /*Console.Write("side=");
+            double side = double.Parse(Console.ReadLine());
+            Console.WriteLine($"P={side * 4}");
+            Console.WriteLine($"S={side * side}");
+            Console.ReadLine();*/
+
+        }
+    }
+}
